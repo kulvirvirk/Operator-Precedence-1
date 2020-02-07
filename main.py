b5eb2395fb40d5ee8cce502794b63f70e622a368
kulvirvirk <kvirkdev@gmail.com>
2020-02-07 04:30:46 +0000

    Added description and comments

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 
+# Guess the output of each answer before you click RUN
+# Try to write down your answer before and see how you do... 
+# use the BODMAS rule - 
+# BODMAS - Bracket, Order, Divide, Multiply, Add, Subtract
 
 print((5 + 4) * 10 / 2)
 print('Answer is: ')
